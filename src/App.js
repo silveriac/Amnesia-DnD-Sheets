051510af50a87e60acb1480bcdbe377568f7544a
silveriac <erixilva42@gmail.com>
2025-05-09 02:18:26 -0300

    added modifier bar

--- src/App.js
+++ src/App.js
@@ -2,65 +2,119 @@ import './App.css';
 import baldric from './baldric.json';
 import { useState } from 'react';
 
-let character = baldric
-console.log(character.ProficiencyBonus);
+const dndSkills = {
+    "Strength": ["Athletics"],
+    "Dexterity": ["Acrobatics", "Sleight of Hand", "Stealth"],
+    "Constitution": [],
+    "Intelligence": ["Arcana", "History", "Investigation", "Nature", "Religion"],
+    "Wisdom": ["Animal Handling", "Insight", "Medicine", "Perception", "Survival"],
+    "Charisma": ["Deception", "Intimidation", "Performance", "Persuasion"]
+  };
+
+let character = baldric; //change this based on queryParams
+//console.log(Object.keys(dndSkills))
+Object.keys(dndSkills).map((element) => {console.log(element)});
 
 const generalStats = [
-        { stat: character.ProficiencyBonus, description: "Proficiency Bonus"},
-        { stat: character.ArmorClass, description: "Armor Class"},
-        { stat: character.Initiative, description: "Initiative"},
-        { stat: character.Speed, description: "Speed"},
-        { stat: character.HitPoints.Max, description: "Hit Points"},
-    ];
+    { stat: character.ProficiencyBonus, description: "Proficiency Bonus" },
+    { stat: character.ArmorClass, description: "Armor Class" },
+    { stat: character.Initiative, description: "Initiative" },
+    { stat: character.Speed, description: "Speed" },
+    { stat: character.HitPoints.Max, description: "Hit Points" },
+];
 
 const roleStats = [
-    { stat: `${character.Class} Level ${character.Level}`, description: "Class"},
-    { stat: character.Background, description: "Background"},
-    { stat: character.Race, description: "Race"},
-    { stat: character.Alignment, description: "Alignment"},
+    { stat: `${character.Class} Level ${character.Level}`, description: "Class" },
+    { stat: character.Background, description: "Background" },
+    { stat: character.Race, description: "Race" },
+    { stat: character.Alignment, description: "Alignment" },
 ];
 
 const characterTraits = [
-    { stat: character.PersonalityTraits, description: "Personality Traits"},
-    { stat: character.Ideals, description: "Ideals"},
-    { stat: character.Bonds, description: "Bonds"},
-    { stat: character.Flaws, description: "Flaws"},
-    { stat: character.Equipment.join("<br/>"), description: "Equipment"},
+    { stat: character.PersonalityTraits, description: "Personality Traits" },
+    { stat: character.Ideals, description: "Ideals" },
+    { stat: character.Bonds, description: "Bonds" },
+    { stat: character.Flaws, description: "Flaws" },
+    { stat: character.Equipment.join("\n"), description: "Equipment" },
 ];
 
 function StatComponent({ stat, description }) {
-    let [shown, setShown] = useState(false);
-    let style = {};
-    if(!shown){
-        style.backgroundColor = "black";
-        style.cursos = "pointer";
-    }
+    const [isVisible, setVisibility, style] = useVisibility(false);
+
     return (
         <div>
-            <div onClick={() => setShown(true)} className="stat" style={style}>
-                {shown? stat : "0"}
+            <div onClick={() => setVisibility(true)} className="stat" style={style}>
+                {isVisible ? stat : "0"}
             </div>
             <div className="description">
                 <p>{description}</p>
             </div>
         </div>
     );
-}
+};
+
+function AttributeComponent({ attribute, description, skillproficiencies, saveproficiencies}) {
+    const modifier = Math.floor((attribute - 10) / 2);
+    //console.log(saveproficiencies.includes("Wisdom"));
+    
+    return (
+        <div className="attribute">
+            <div>
+                <span className="modifier">{modifier}</span>
+                <span className="core-stat">{attribute}</span>
+                <span className="description">{description}</span>
+            </div>
+            <ul>
+                <SkillComponent
+                        skillStat={saveproficiencies.includes(attribute) ? modifier + character.ProficiencyBonus : modifier}
+                        skillName= "Saving Throw"
+                />
+                {dndSkills[description].map((element, index) =>{
+                    return(
+                        <SkillComponent
+                        skillStat={skillproficiencies.includes(element) ? modifier + character.ProficiencyBonus : modifier}
+                        skillName={element}
+                        />
+                    )
+                })}
+            </ul>
+        </div>
+    );
+};
+
+function SkillComponent({skillStat, skillName}) {
+    return (
+        <li>
+            <span className="stat-horizontal">
+                {skillStat}
+            </span>
+            <span>{skillName}</span>
+        </li>
+    );
+};
+
+function useVisibility(initial = false) {
+    const [isVisible, setVisibility] = useState(initial);
+
+    const style = isVisible ? {} : {
+        backgroundColor: "black",
+        cursor: "pointer",
+    };
+
+    return [isVisible, setVisibility, style];
+};
 
 function App() {
     return (
         <div className="App">
             <header>
                 <section id="nameContainer">
-                    <div id="name" className="stat"></div>
-                    <div className="description">
-                        <p>Character Name</p>
-                    </div>
+                    <StatComponent stat={character.CharacterName} description="Character Name" />
                 </section>
                 <section id="roleContainer">
                     {roleStats.map(element => (
-                            <StatComponent stat={element.stat} description={element.description} />
-                        ))}
+                        <StatComponent stat={element.stat} description={element.description} />
+                    ))}
                 </section>
             </header>
             <main>
@@ -73,70 +127,21 @@ function App() {
 
                     <section id="leftBottom">
                         <section id="attributeSection">
-                        <div class="attribute">
-                        <div>
-                            <span id="dexterityMOD" class="modifier">ff</span>
-                            <span id="dexterity" class="core-stat">15</span>
-                            <span class="description">dexterity</span>
-                        </div>
-                        <ul id="dexterityList">
-                        <li>
-                            <span id="dexteritySave" class="stat-horizontal"><input type="radio" disabled=""/>
-
-                            </span>
-                            <label for="dexteritySave">Saving throw</label>
-                        </li><li>
-                        <span id="actobatics" class="stat-horizontal"><input type="radio" disabled=""/>
-                            2
-                        </span>
-                        <label for="actobatics">actobatics</label>
-                    </li><li>
-                        <span id="sleightOfHand" class="stat-horizontal"><input type="radio" disabled=""/>
-                            2
-                        </span>
-                        <label for="sleightOfHand">sleight of hand</label>
-                    </li><li>
-                        <span id="stealth" class="stat-horizontal"><input type="radio" disabled=""/>
-                            4
-                        </span>
-                        <label for="stealth">stealth</label>
-                    </li></ul>
-                    </div>
+                            {Object.keys(dndSkills).map((element) =>  (
+                                //console.log(element);
+                                
+                                <AttributeComponent
+                                    attribute={character.Attributes[element]}
+                                    description={element}
+                                    skillproficiencies={character.SkillProficiencies}
+                                    saveproficiencies={character.SaveProficiencies}
+                                />
+                            ))}
                         </section>
                         <section id="characterTraits">
                             {characterTraits.map(element => (
                                 <StatComponent stat={element.stat} description={element.description} />
                             ))}
-                            <div>
-                                <div id="personalityTraits" classNameName="stat"></div>
-                                <div classNameName="description">
-                                    <p>personality traits</p>
-                                </div>
-                            </div>
-                            <div>
-                                <div id="ideals" classNameName="stat"></div>
-                                <div classNameName="description">
-                                    <p>ideals</p>
-                                </div>
-                            </div>
-                            <div>
-                                <div id="bonds" classNameName="stat"></div>
-                                <div classNameName="description">
-                                    <p>bonds</p>
-                                </div>
-                            </div>
-                            <div>
-                                <div id="flaws" classNameName="stat"></div>
-                                <div classNameName="description">
-                                    <p>flaws</p>
-                                </div>
-                            </div>
-                            <div>
-                                <div id="equipment" classNameName="stat"></div>
-                                <div classNameName="description">
-                                    <p>equipment</p>
-                                </div>
-                            </div>
                         </section>
                     </section>
                 </section>
